3a773ef40352b2aac415fa30e4c56486d5b1da88
KairatSabiyev <sabiyev@gmail.com>
2022-10-06 19:54:05 -0400

    updated gitignore and WebDriverFactory files

--- src/test/java/com/vytrack/utilities/WebDriverFactory.java
+++ src/test/java/com/vytrack/utilities/WebDriverFactory.java
@@ -1,2 +1,31 @@
-package com.vytrack.utilities;public class WebDriverFactory {
+package com.vytrack.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverFactory {
+
+    public static WebDriver getDriver(String browserType){  // "Chrome""
+
+        if(browserType.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        } else if(browserType.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if(browserType.equalsIgnoreCase("edge")){
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+
+        } else {
+            System.out.println("Given browser type doesn't exist/or not currently supported!");
+            return null;
+        }
+
+    }
+
 }
